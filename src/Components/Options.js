f3729345d5b75df54f637dba0d3529bf21297037
himanshush200599 <himanshush214@gmail.com>
2018-09-06 13:46:05 -0400

    add methods bind

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,8 +1,17 @@
 import React , { Component } from 'react';
 import Option from './Option';
 export default class Options extends Component {
+  constructor(props){
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    //bind reset the function context again by calling it gain
+    //in this situation function handleRemoveAll loses its context so we
+    //bind it to this.
+    //By this we can now use props passes to parent in function methods also
+    //thus bind every method in constructor .
+  }
   handleRemoveAll(){
-    alert("Remove all")
+    alert(this.props.options)
   }
   render(){
     return(

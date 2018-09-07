36b6dd51ed30a38418f087a639a75b411913719f
himanshush200599 <himanshush214@gmail.com>
2018-09-07 00:59:31 -0400

    make add option form validation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.handleDeletesOptions = this.handleDeletesOptions.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
     this.handlePick = this.handlePick.bind(this);
     this.state={
       options :  ['banana','orange','apple']
@@ -25,6 +26,20 @@ handlePick(){
   const rand = Math.round(Math.random()*this.state.options.length)
   alert(this.state.options[rand])
 }
+handleAddOption(option){
+  if(!option){
+    return "This feild cannot be empty";
+  }
+  else if (this.state.options.indexOf(option)>-1){
+    return "This item all ready exists";
+  }
+  this.setState((prevState)=>{
+    return{
+      options: prevState.options.concat([option])
+    };
+  });
+
+}
   render() {
     const title = ' Indecision-App';
     const subtitle = 'Put your life in the hands of computer';
@@ -36,7 +51,7 @@ handlePick(){
           handlePick={this.handlePick}/>
         <Options options={this.state.options}
           handleDeletesOptions={this.handleDeletesOptions} />
-        <AddOption />
+        <AddOption handleAddOption={this.handleAddOption}/>
       </div>
     );
   }
--- src/Components/AddOption.js
+++ src/Components/AddOption.js
@@ -1,19 +1,27 @@
 import React , { Component } from 'react';
 export default class AddOption extends Component {
+  constructor(props){
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state={
+      error:undefined
+    }
+  }
   handleAddOption(e){
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
      //trim removes white spaces from string
-    if(option){
-      alert(option)
-    }
-    else{
-      alert("Add some text in input feild")
-    }
+     const error = this.props.handleAddOption(option);
+     this.setState(()=>{
+       return{
+         error //this is similar to error:error
+       }
+     })
   }
   render(){
     return(
       <div>
+        {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
             <input type="text" name='option' />
             <button type="submit">Add Option</button>
